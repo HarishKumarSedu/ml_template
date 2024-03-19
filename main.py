8a3eb8e85f3428d4a76bc40d1e0be68205cdcf6a
Harish <harishkumarsedu@gmail.com>
2024-03-19 17:06:52 +0100

    model trainer

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from src.mlProject import logger
-from src.mlProject.pipeline.stage_01_dataingestion_pipeline import DataIngestionTrainingPipeline
-from src.mlProject.pipeline.stage_02_datavalidation_pipeline import DataValidationTrainingPipeline
+from src.mlProject.pipeline.stage_01_data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.mlProject.pipeline.stage_02_data_validation_pipeline import DataValidationTrainingPipeline
+from src.mlProject.pipeline.stage_03_data_transformation_pipeline import DataTransformationTrainingPipeline
+from src.mlProject.pipeline.stage_04_model_trainer_pipeline import ModelTrainerTrainingPipeline
 
 
 
@@ -8,14 +10,26 @@ if __name__ == '__main__':
     try:
         STAGE_NAME = 'DATA INGESTION '
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = DataIngestionTrainingPipeline()
-        obj.main()
+        objDataIngestion = DataIngestionTrainingPipeline()
+        objDataIngestion.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 
         STAGE_NAME = 'DATA VADLIDATION '
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = DataValidationTrainingPipeline()
-        obj.main()
+        objDataValidation = DataValidationTrainingPipeline()
+        objDataValidation.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+        STAGE_NAME = 'DATA TRANSFORMATION '
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        objDataTransformation = DataTransformationTrainingPipeline()
+        objDataTransformation.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+        STAGE_NAME = 'MODEL TRAINING '
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        objModelTrain = ModelTrainerTrainingPipeline()
+        objModelTrain.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
         
     except Exception as e:
--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -1,5 +1,7 @@
 from src.mlProject.entity.config_entity import (DataIngestionConfig,
                                                 DataValidationConfig,
+                                                DataTransformationConfig,
+                                                ModelTrainConfig
                                                 )
 from src.mlProject import logger
 from src.mlProject.constants import *
@@ -45,3 +47,32 @@ class ConfigurationManager:
             all_schema      =schema
         )
         return data_validation
+
+    def get_data_transformation_config(self)-> DataTransformationConfig:
+        
+        config = self.config.data_transfromation
+        create_directories([config.root_dir])
+        
+        data_transformation = DataTransformationConfig(
+            root_dir= config.root_dir,
+            data_path= config.data_path
+        )
+        return data_transformation
+    
+    def get_model_trainer_config(self)->ModelTrainConfig:
+        config = self.config.model_trainer
+        target = self.schema.TARGET_COLUMN
+        params = self.params.ElasticNet
+        create_directories([config.root_dir])
+        
+        model_trainer = ModelTrainConfig(
+                    root_dir            = config.root_dir  ,
+                    train_data_path     = config.train_data_path,
+                    test_data_path      = config.test_data_path,
+                    model_name          = config.model_name,
+                    alpha               = params.alpha,
+                    l1_ratio            = params.l1_ratio,
+                    target_column       = target.name,
+        )
+        
+        return model_trainer
