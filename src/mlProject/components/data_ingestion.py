0f6a9abb945f8e1e09a78ee381c1cbcf4c7e51bd
Harish <harishkumarsedu@gmail.com>
2024-03-19 13:07:52 +0100

    data ingestion component added

---
+++ src/mlProject/components/data_ingestion.py
@@ -0,0 +1,36 @@
+import os 
+import urllib.request as request
+import zipfile
+from pathlib import Path 
+from src.mlProject.utils.common import get_size
+from src.mlProject.entity.config_entity import DataIngestionConfig
+from src.mlProject import logger 
+
+class DataIngestion:
+
+    def __init__(self,config:DataIngestionConfig) -> None:
+        self.config = config 
+
+    def download_file(self):
+
+        if not os.path.exists(self.config.local_data_file):
+            filename, header = request.urlretrieve(
+                url=self.config.source_url,
+                filename=self.config.local_data_file
+            )
+
+            logger.info(f' {filename} downloaded! with the following infor \n{header}')
+        else:
+            logger.info(f' {filename} already exists with size of {get_size(Path(self.config.local_data_file))} ')
+    
+    def extract_zip_file(self):
+        unzip_path = self.config.unzip_dir
+        os.makedirs(unzip_path, exist_ok=True)
+        _, filename = os.path.split(self.config.local_data_file)
+        if os.path.exists(self.config.local_data_file):
+            with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_file :
+                zip_file.extractall(unzip_path)
+                logger.info(f' {filename} unziped inside the {Path(self.config.unzip_dir)}')
+        else:
+            logger.info(f' data {filename} dose not exists in  {Path(self.config.local_data_file)}')
+
